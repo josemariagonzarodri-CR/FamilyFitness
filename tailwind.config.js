0fbc909067cce5c39fbede6e8773844b1a3cd4fc
Chemalpine <chamalpineluv@icloud.com>
2026-03-16 19:40:37 -0600

    fix: correccion de purga de Tailwind en produccion

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,16 +5,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        titanium: {
-          900: '#0A1128',
-          800: '#003F88',
-          accent: '#00F2FE',
-        },
-        glass: 'rgba(255, 255, 255, 0.05)',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
